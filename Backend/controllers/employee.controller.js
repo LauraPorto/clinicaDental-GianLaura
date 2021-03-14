5deae96ba9a792482ac7ebb3f1767819a962e1c7
Giancarlo <giancarlorondo@outlook.com>
2021-03-14 13:27:33 +0100

    changing roles of employee and patient

--- Backend/controllers/employee.controller.js
+++ Backend/controllers/employee.controller.js
@@ -1,15 +1,70 @@
 const {Employee} = require('../models');
 
+//bcrypt para encriptar psw 
+const bcrypt = require('bcrypt');
+//jwt para firmar token
+const jwt = require('jsonwebtoken');
+const secret = process.env.JWT_SECRET || 'pwsSecret';
+
+
 class EmployeeController {
     constructor(){}
 
-    async indexAll(){
-        return Employee.findAll();
-    }
+    // async indexAll(){
+    //     return Employee.findAll();
+    // }
 
     async findById(id){
-        return Employee.findOne({where:{id}});
+         return Employee.findOne({where:{id}});
     }
+
+    async register(employee){
+        //Encrypto la contrasela de employee
+        employee.password = await bcrypt.hash(employee.password,6);
+        return Employee.create(employee);
+    }
+
+    async logIn (email, password){
+        //############Añadiríamos el parámetro admin de employee
+        //Busco el employee por email
+        const employee = await Employee.findOne({where:{email}});
+
+        if(!employee){
+            return null;
+        }
+
+        if(!await bcrypt.compare(password, employee.password)){
+            //Le devuelvo un null para no pasarle detalles de error y facilitar inferir usuario y contraseña
+            return null;
+        }
+
+        //informacion guardada en el token
+        const payload = {
+            employeeId : employee.id,
+            tokenCreationDate: new Date
+            //admin: employee.admin
+            
+            //#################propiedad de payload para poder firmar el token mediante el booleano true(employee), false (pacient)
+            //Añadir un require en los modelos de empleado y paciente??
+
+            //Le puedo poner la informacion que quiera, como el rol del usuario (adm, permisos...)
+        };
+
+
+        //Devuelvo un json del payload firmado
+        return jwt.sign(payload, secret); 
+    }
+
+    async logOut (){}
+
+    async update(employee, id){
+        return Employee.update(employee, {where:{id}});
+    }
+
+    async deleteEmployee (id){
+        return Employee.destroy({where:{id}});
+    }
+
 
 }
 
--- Backend/routes/employee.router.js
+++ Backend/routes/employee.router.js
@@ -3,10 +3,30 @@ const employeeController = require('../controllers/employee.controller');
 
 //ENDPOINTS
 
-//GET para consultar todas las citas
-router.get('/all', async (req, res) => {
+//POST para registrarse
+router.post('/register', async (req, res) => {
     try{
-        res.json(await employeeController.indexAll());
+        let status = 'Employee created';
+        let newEmployee = await employeeController.register(req.body); 
+        res.json({newEmployee, status});
+    }catch(error){
+        res.status(500).json({
+            message: 'Server Error' + error
+        });
+    };
+});
+
+//GET para id employee
+router.get('/:id', async (req, res) => {
+    try{
+        let id = req.params.id;
+        let employee = await employeeController.findById(id);
+        if(employee){
+            res.json(employee);
+        }else{
+            res.sendStatus(404);
+        }
+        
     }catch(error){
         res.status(500).json({
             message: 'Server Error' + error
@@ -26,6 +46,41 @@ router.get('/:id', async (req, res) => {
     };
 });
 
+//POST para login
+//Cambio a Post para no pasar la contraseña por la URL, no queda en el historial
+router.post('/login', async (req, res) => {
+    try{
+        
+        let email = req.body.email;
+        let password = req.body.password;
+
+        console.log('Comprobación del post login', email, password);
+        
+        let token = await employeeController.logIn(email, password); 
+        
+        if(token){
+            res.json({token});
+        }else{
+            res.sendStatus(403);
+        }
+        
+    }catch(error){
+        res.status(500).json({
+            message: 'Server Error' + error
+        });
+    };
+});
+
+//GET para logout
+router.get('/logout', async (req, res) => {
+    try{
+        res.sendStatus(200);
+    }catch(error){
+        res.status(500).json({
+            message: 'Server Error' + error
+        });
+    };
+});
 
 
 module.exports = router;
